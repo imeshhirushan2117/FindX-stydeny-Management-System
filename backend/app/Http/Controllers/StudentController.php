2d43120814e00df8863cbd60bd2a4ba5c4c2b689
imeshhirushan2117 <imeshhirushan2117@gmail.com>
2025-08-05 23:33:24 +0530

    post API

--- backend/app/Http/Controllers/StudentController.php
+++ backend/app/Http/Controllers/StudentController.php
@@ -11,4 +11,17 @@ class StudentController extends Controller
     {
         return Student::all();
     }
+
+    public function store(Request $request)
+    {
+        $validatedData = $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|unique:students,email',
+            'birthdate' => 'required|date',
+            'city' => 'required|string|max:255',
+            'contact_number' => 'required|string|max:15',
+        ]);
+
+        return Student::create($validatedData);
+    }
 }
