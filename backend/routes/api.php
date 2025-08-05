0c92175871793664e2552d02518ca2fd31abf749
imeshhirushan2117 <imeshhirushan2117@gmail.com>
2025-08-05 23:18:33 +0530

    get student Api

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -14,4 +14,6 @@ Route::post('/login', [AuthController::class, 'login']);
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/user', [AuthController::class, 'user']);
     Route::post('/logout', [AuthController::class, 'logout']);
+
+    Route::apiResource('students', \App\Http\Controllers\StudentController::class);
 });
